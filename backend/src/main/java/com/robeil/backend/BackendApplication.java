20c4cb48059a2a1a025a0abbb845d22dfd177a30
Robeil Aregawi <silukeen1@gmail.com>
2022-07-17 14:59:51 -0500

    added

--- backend/src/main/java/com/robeil/backend/BackendApplication.java
+++ backend/src/main/java/com/robeil/backend/BackendApplication.java
@@ -1,13 +1,30 @@
 package com.robeil.backend;
 
 
+import com.robeil.backend.entity.Contact;
+import com.robeil.backend.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+
+    private ContactRepository contactRepository;
+    @Autowired
+    public BackendApplication(ContactRepository contactRepository){
+        this.contactRepository = contactRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        if(contactRepository.findAll().isEmpty()){
+            contactRepository.save(new Contact(1,2066025164,"hi there","robeilaregawi@gmail.com"));
+        }
+    }
 }
--- backend/src/main/java/com/robeil/backend/repository/ContactRepository.java
+++ backend/src/main/java/com/robeil/backend/repository/ContactRepository.java
@@ -2,9 +2,12 @@ package com.robeil.backend.repository;
 
 import com.robeil.backend.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+//@Repository
+//public interface ContactRepository extends JpaRepository<Contact, Long> {
 @Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> {
+public interface ContactRepository extends MongoRepository<Contact, Long> {
 
 }
